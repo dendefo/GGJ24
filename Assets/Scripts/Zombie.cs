6e70f1b55052109a8f5da4d6d7811d290e5f4e8d
dendefo <daniek332@gmail.com>
2024-01-25 21:46:31 +0200

    Push Zombie

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Zombie : MonoBehaviour
 {
-
+    [SerializeField] Rigidbody2D leftHand;
+    [SerializeField] float speed;
     public delegate void ZombieStick(Zombie zombie);
     public static event ZombieStick OnZombieStick;
     private void Awake()
@@ -15,5 +16,12 @@ public class Zombie : MonoBehaviour
     public void Stick()
     {
         OnZombieStick?.Invoke(this);
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            leftHand.AddForce(Vector2.up * speed,ForceMode2D.Impulse);
+        }
     }
 }
