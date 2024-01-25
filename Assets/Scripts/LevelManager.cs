30672aa7edced6415ca743a815aea55a926292af
dendefo <daniek332@gmail.com>
2024-01-25 22:04:39 +0200

    push

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -12,7 +12,6 @@ public class LevelManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
