a7e4778e6f8e274b576b3d976562c0e51c3a86c8
dendefo <daniek332@gmail.com>
2024-01-27 14:08:29 +0200

    push

--- Assets/Scripts/ParalaxedObject.cs
+++ Assets/Scripts/ParalaxedObject.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
-
+#endif
 [ExecuteInEditMode]
 public class ParalaxedObject : MonoBehaviour
 {
     public Vector3 InitialPosition;
     public BackgroundManager BackgroundManager;
 
+#if UNITY_EDITOR
     void Update()
     {
         if (Selection.activeTransform == this.transform)
@@ -16,4 +18,5 @@ public class ParalaxedObject : MonoBehaviour
             InitialPosition = transform.position - new Vector3(BackgroundManager.Camera.position.x, BackgroundManager.Camera.position.y, 0) * (transform.position.z/100);
         }
     }
+#endif
 }
